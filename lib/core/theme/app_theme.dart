7e9d6eaafbc1fefbee0e2ffe881139db918cf88a
WalMaa <98175031+WalMaa@users.noreply.github.com>
2025-03-08 11:19:35 +0200

    Refactor theme management to use a unified text theme builder; update light and dark themes for consistency and improved readability.

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,144 +2,134 @@ import 'package:flutter/material.dart';
 import 'theme_constants.dart';
 
 enum AppTheme {
-  Light,
-  Dark,
+  light,
+  dark,
 }
 
 const Color primaryColor = Color.fromARGB(255, 35, 64, 141);
 
 class AppThemeData {
+  // Common text theme settings
+  static TextTheme _buildTextTheme(Color textColor) {
+    return TextTheme(
+      displayLarge: TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.bold,
+        color: textColor,
+      ),
+      displayMedium: TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.bold,
+        color: textColor,
+      ),
+      displaySmall: TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.bold,
+        color: textColor,
+      ),
+      headlineMedium: TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.bold,
+        color: textColor,
+      ),
+      headlineSmall: TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.bold,
+        color: textColor,
+      ),
+      titleLarge: TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.bold,
+        color: textColor,
+      ),
+      titleMedium: TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w400,
+        color: textColor,
+      ),
+      titleSmall: TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w400,
+        color: textColor,
+      ),
+      bodyLarge: TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w400,
+        color: textColor,
+      ),
+      bodyMedium: TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w400,
+        color: textColor,
+      ),
+      bodySmall: TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w300,
+        color: textColor,
+      ),
+      labelLarge: TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w500,
+        color: textColor,
+      ),
+      labelSmall: TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w400,
+        color: textColor,
+      ),
+    );
+  }
+
+  // Light theme definition
   static final lightTheme = ThemeData.light().copyWith(
     scaffoldBackgroundColor: AppPallete.whiteColor,
     primaryColor: primaryColor,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: primaryColor,
+      brightness: Brightness.light,
+    ),
     appBarTheme: const AppBarTheme(
       backgroundColor: Color.fromARGB(255, 242, 242, 242),
       actionsIconTheme: IconThemeData(color: Colors.black),
       iconTheme: IconThemeData(color: Colors.black),
       titleTextStyle: TextStyle(
-          color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold),
+        color: Colors.black,
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+      ),
     ),
-    textTheme: const TextTheme(
-      displayLarge: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.bold,
-          color: Colors.black),
-      displayMedium: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.bold,
-          color: Colors.black),
-      displaySmall: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.bold,
-          color: Colors.black),
-      headlineMedium: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.bold,
-          color: Colors.black),
-      headlineSmall: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.bold,
-          color: Colors.black),
-      titleLarge: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.bold,
-          color: Colors.black),
-      titleMedium: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w400,
-          color: Colors.black),
-      titleSmall: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w400,
-          color: Colors.black),
-      bodyLarge: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w400,
-          color: Colors.black),
-      bodyMedium: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w400,
-          color: Colors.black),
-      bodySmall: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w300,
-          color: Colors.black),
-      labelLarge: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w500,
-          color: Colors.black),
-      labelSmall: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w400,
-          color: Colors.black),
-    ),
+    textTheme: _buildTextTheme(Colors.black),
   );
 
+  // Dark theme definition
   static final darkTheme = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: AppPallete.backgroundColor,
-    primaryColor: Colors.indigo,
-    colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo)
-        .copyWith(secondary: Colors.indigoAccent),
+    primaryColor: primaryColor, // Use the same primary color
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: primaryColor,
+      brightness: Brightness.dark,
+    ),
     appBarTheme: const AppBarTheme(
       backgroundColor: AppPallete.backgroundColor,
       actionsIconTheme: IconThemeData(color: Colors.white),
       iconTheme: IconThemeData(color: Colors.white),
       titleTextStyle: TextStyle(
-          color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
+        color: Colors.white,
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+      ),
     ),
-    textTheme: const TextTheme(
-      displayLarge: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.bold,
-          color: Colors.white),
-      displayMedium: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.bold,
-          color: Colors.white),
-      displaySmall: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.bold,
-          color: Colors.white),
-      headlineMedium: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.bold,
-          color: Colors.white),
-      headlineSmall: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.bold,
-          color: Colors.white),
-      titleLarge: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.bold,
-          color: Colors.white),
-      titleMedium: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w400,
-          color: Colors.white),
-      titleSmall: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w400,
-          color: Colors.white),
-      bodyLarge: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w400,
-          color: Colors.white),
-      bodyMedium: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w400,
-          color: Colors.white),
-      bodySmall: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w300,
-          color: Colors.white),
-      labelLarge: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w500,
-          color: Colors.white),
-      labelSmall: TextStyle(
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w400,
-          color: Colors.white),
-    ),
+    textTheme: _buildTextTheme(Colors.white),
   );
+
+  // Helper method to get theme based on enum
+  static ThemeData getThemeData(AppTheme theme) {
+    switch (theme) {
+      case AppTheme.dark:
+        return darkTheme;
+      case AppTheme.light:
+      default:
+        return lightTheme;
+    }
+  }
 }
